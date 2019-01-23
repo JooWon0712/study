2e4812bf6d28755da922c09040ebc3adf5936e3f
서주원 <destiny0712@naver.com>
2019-01-23 23:02:35 +0900

    JPA 프로그래밍7. Query 공부 - JPQL(HQL) - Criteria - Native Query

--- jpa/src/main/java/joowon/study/jpa/Account/JpaRunner.java
+++ jpa/src/main/java/joowon/study/jpa/Account/JpaRunner.java
@@ -1,6 +1,5 @@
 package joowon.study.jpa.Account;
 
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -8,6 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Component
 @Transactional
@@ -60,18 +65,39 @@ public class JpaRunner implements ApplicationRunner {
 //        session.delete(post1);
 
         /* 4번 */
-        Session session = entityManager.unwrap(Session.class);
-        Post post = session.get(Post.class, 7l);
-        Comment comment = session.get(Comment.class, 8l);
-        System.out.println("========================");
-        System.out.println(post.getTitle());
-        System.out.println("=====comments======");
-        post.getComments().forEach(a -> {
-            System.out.println(a.getComment());
-        });
+//        Session session = entityManager.unwrap(Session.class);
+//        Post post = session.get(Post.class, 7l);
+//        Comment comment = session.get(Comment.class, 8l);
+//        System.out.println("========================");
+//        System.out.println(post.getTitle());
+//        System.out.println("=====comments======");
+//        post.getComments().forEach(a -> {
+//            System.out.println(a.getComment());
+//        });
 
 //        System.out.println(comment.getPost().getTitle());
 //        System.out.println(comment.getComment());
+
+        /* 5번 (JPQL) */
+        /*
+        딘점 : Type safe하지 못하다.
+         */
+//        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
+//        List<Post> posts = query.getResultList();
+//        posts.forEach(System.out::println);
+
+        /* 6번 (Criteria) */
+//        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+//        Root<Post> root = query.from(Post.class);
+//        query.select(root);
+//
+//        List<Post> posts = entityManager.createQuery(query).getResultList();
+//        posts.forEach(System.out::println);
+
+        /* 7번 (Native Query */
+        List<Post> posts = entityManager.createNativeQuery("SELECT * FROM Post", Post.class).getResultList();
+        posts.forEach(System.out::println);
 
     }
 }
