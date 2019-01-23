daadb1bd6de63022579353bda62da825a29fba7c
서주원 <destiny0712@naver.com>
2019-01-23 23:28:07 +0900

    스프링 데이터 JPA 소개 및 원리
    시작은 @Import(JpaRepositoriesRegistrar.class)
    핵심은 ImportBeanDefinitionRegistrar 인터페이스

--- jpa/src/main/java/joowon/study/jpa/Account/JpaRunner.java
+++ jpa/src/main/java/joowon/study/jpa/Account/JpaRunner.java
@@ -1,5 +1,6 @@
 package joowon.study.jpa.Account;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,11 @@ import java.util.List;
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager entityManager;
+//    @PersistenceContext
+//    EntityManager entityManager;
+
+    @Autowired
+    PostRepository postRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -96,8 +100,11 @@ public class JpaRunner implements ApplicationRunner {
 //        posts.forEach(System.out::println);
 
         /* 7번 (Native Query */
-        List<Post> posts = entityManager.createNativeQuery("SELECT * FROM Post", Post.class).getResultList();
-        posts.forEach(System.out::println);
+//        List<Post> posts = entityManager.createNativeQuery("SELECT * FROM Post", Post.class).getResultList();
+//        posts.forEach(System.out::println);
+
+        /* 8번 Spring Data JPA 사용 */
+        postRepository.findAll().forEach(System.out::println);
 
     }
 }
---
+++ jpa/src/main/java/joowon/study/jpa/Account/PostRepository.java
@@ -0,0 +1,7 @@
+package joowon.study.jpa.Account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
