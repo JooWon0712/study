45775f9ce87aaffa218ed12f0b5b64935f1c9da2
서주원 <destiny0712@naver.com>
2019-01-25 01:26:04 +0900

    스프링 데이터 Common 11. Web - Pageable, Sort 알아보기

--- jpa/src/main/java/joowon/study/jpa/web/WebPostController.java
+++ jpa/src/main/java/joowon/study/jpa/web/WebPostController.java
@@ -1,6 +1,8 @@
 package joowon.study.jpa.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebPostController {
 
     @Autowired
-    WebPostRepository webPostRepository;
+    WebPostRepository posts;
 
     @GetMapping("/post/{id}")
     public String getWebPost(@PathVariable("id") WebPost webPost) {
         return webPost.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<WebPost> getWebPosts(Pageable pageable) {
+        return posts.findAll(pageable);
     }
 }
