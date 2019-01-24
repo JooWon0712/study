3f3abd108d79804bb78f32439de5f8887944cef0
서주원 <destiny0712@naver.com>
2019-01-24 22:31:14 +0900

    스프링 데이터 Common 9. QueryDSL 연동하기 공부.

--- jpa/src/main/java/joowon/study/jpa/newpost/NewPostRepository.java
+++ jpa/src/main/java/joowon/study/jpa/newpost/NewPostRepository.java
@@ -1,7 +1,8 @@
 package joowon.study.jpa.newpost;
 
 import joowon.study.jpa.commonrepository.MyRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface NewPostRepository extends MyRepository<NewPost, Long> {
+public interface NewPostRepository extends MyRepository<NewPost, Long>, QuerydslPredicateExecutor<NewPost> {
 
 }
--- jpa/src/test/java/joowon/study/jpa/newpost/NewPostRepositoryTest.java
+++ jpa/src/test/java/joowon/study/jpa/newpost/NewPostRepositoryTest.java
@@ -1,11 +1,15 @@
 package joowon.study.jpa.newpost;
 
+import com.querydsl.core.types.Predicate;
+import joowon.study.jpa.account.QPost;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,6 +35,18 @@ public class NewPostRepositoryTest {
 
         newPostRepository.delete(newPost);
         newPostRepository.flush();
+
+    }
+
+    @Test
+    public void crud2() {
+        NewPost newPost = new NewPost();
+        newPost.setTitle("hiber nate");
+        newPostRepository.save(newPost.publish());
+
+        Predicate predicate = QNewPost.newPost.title.containsIgnoreCase("hiber nate");
+        Optional<NewPost> one = newPostRepository.findOne(predicate);
+        assertThat(one).isNotEmpty();
 
     }
 
