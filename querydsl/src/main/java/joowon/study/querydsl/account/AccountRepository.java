6034e71494b0b936261734e93e354f146927d72c
서주원 <destiny0712@naver.com>
2019-01-24 22:03:17 +0900

    스프링 데이터 Common 9. QueryDSL 데모 프로젝트 생성

---
+++ querydsl/src/main/java/joowon/study/querydsl/account/AccountRepository.java
@@ -0,0 +1,7 @@
+package joowon.study.querydsl.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
