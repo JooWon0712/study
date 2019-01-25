14247a0d5d16938e5bf97fc6439467a406b32afe
서주원 <destiny0712@naver.com>
2019-01-26 02:41:06 +0900

    스프링 데이터 JPA 10. 트랜잭션 알아보기

--- jpa/src/main/java/joowon/study/jpa/newcomment/NewCommentRepository.java
+++ jpa/src/main/java/joowon/study/jpa/newcomment/NewCommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +16,6 @@ public interface NewCommentRepository extends JpaRepository<NewComment, Long>
     @EntityGraph(attributePaths = "newPost")
     Optional<NewComment> getById(Long id);
 
+    @Transactional(readOnly = true)
     <T> List<T> findByNewPost_Id(Long id, Class<T> type);
 }
