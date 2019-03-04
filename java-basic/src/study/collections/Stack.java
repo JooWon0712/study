1600938f83c5db9c6fcc304df84740082d7859ed
서주원 <destiny0712@naver.com>
2019-03-04 19:46:13 +0900

    주석 추가

--- java-basic/src/study/collections/Stack.java
+++ java-basic/src/study/collections/Stack.java
@@ -21,6 +21,10 @@ public class Stack {
        this.size = size;
     }
 
+    // 스택에서 데이터 꺼내기
+    // 스택에서 데이터를 꺼낼때 꺼낸 데이터는 remove 메소드를 통해 제거해주었다.
+    // 스택에서 꺼낼 데이터가 없을 경우 Exception 처리해야한다.
+    // (테스트코드에선 System.out.println 으로 처리함)
     private void pop() {
         if (this.top == -1) {
             System.out.println("Exception : stack no elements");
@@ -33,6 +37,9 @@ public class Stack {
         this.top = this.stack.size() - 1;
     }
 
+    // 스택에 데이터 입력하기
+    // 스택에 사이즈를 초과할 경우 Exception 처리해야한다.
+    // (테스트코드에선 System.out.println 으로 처리함)
     private void push(Object object) {
         if (this.size == this.stack.size()) {
             System.out.println("Exception : Stack full");
@@ -44,10 +51,13 @@ public class Stack {
         this.stack.add(object);
     }
 
+    // 스택에서 젤 마지막에 들어있는 데이터 확인하기
     private void peek() {
         System.out.println("== PEEK ("+this.stack.get(this.top)+") ==");
     }
 
+    // 스택에 쌓여있는 데이터 출력하기
+    // 젤 마지막 데이터부터 출력
     private void print() {
         System.out.println("## Stack print start");
         for (int i = this.stack.size()-1; i >= 0; i--) {
